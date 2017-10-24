986d1564af9d213fa2e3a7077f2bbfc1ede2b6c0
Aleksei Waddington <Aleksei Waddington>
2017-10-24 12:01:15 -0400

    apps is now fully comented

--- Assets/Scripts/Apps.cs
+++ Assets/Scripts/Apps.cs
@@ -5,32 +5,44 @@ using UnityEngine.UI;
 
 public class Apps { 
 	
-    private GameObject p_Window;
-    private TextAsset p_theText;
-    private string p_TextArray;
-    private Text p_Text;
-    private Button p_Exit;
-    private bool p_isDisplayed;
+    private GameObject p_Window; //reference to the window gameobject
+    private TextAsset p_theText; //reference to the text file that contains the text that will be displayed
+    private string p_TextArray; //reference to the string of text that will contain the contents of the textfile
+    private Text p_Text; //the text object in the scene
+    private Button p_Exit; //the button to close the window
+    private bool p_isDisplayed; //is the window currently being displayed
 
-
+    /// <summary>
+    /// creates a window that will display text within it
+    /// </summary>
+    /// <param name="window"></param>
+    /// <param name="theText"></param>
+    /// <param name="canvas"></param>
     public Apps(GameObject window, TextAsset theText, GameObject canvas)
     {
-        p_Window = GameObject.Instantiate(window, canvas.transform);
-        p_Window.GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(-394, 471), Random.Range(-187, 244));
-        p_theText = theText;
-        p_Text = p_Window.gameObject.GetComponentInChildren<Text>();
-        p_Exit = p_Window.gameObject.GetComponentInChildren<Button>();
-        p_TextArray = p_theText.text;
-        p_Text.text = p_TextArray;
-        p_isDisplayed = true;
-        p_Exit.onClick.AddListener(setWindowOff);
-    }
-    void setWindowOff()
-    {
-        p_Window.SetActive(false);
-        p_isDisplayed = false;
+        p_Window = GameObject.Instantiate(window, canvas.transform); //create the window from the prefab object, have it spawn under the canvas
+        p_Window.GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(-394, 471), Random.Range(-187, 244)); // randomly set the location of the window in the scene
+        p_theText = theText; //get a reference to the test file
+        p_Text = p_Window.gameObject.GetComponentInChildren<Text>(); //Find the relevant text object in the scene
+        p_Exit = p_Window.gameObject.GetComponentInChildren<Button>(); //find th relevant exit button in the scene
+        p_TextArray = p_theText.text; //store the text in the string
+        p_Text.text = p_TextArray; //have the string of text display on the screen
+        p_isDisplayed = true; //the window is being displayed
+        p_Exit.onClick.AddListener(setWindowOff); //the buton can now close itself
     }
 
+    /// <summary>
+    /// function that is called when the player wants to close a window
+    /// </summary>
+    void setWindowOff()
+    {
+        p_Window.SetActive(false); //set the window to inactive
+        p_isDisplayed = false; //update the bool to reflect this
+    }
+
+    /// <summary>
+    /// getter and setter for the bool that checks if the window is being displayed
+    /// </summary>
     public bool isDisplayed
     {
         get
@@ -42,6 +54,9 @@ public class Apps {
             this.p_isDisplayed = value;
         }
     }
+    /// <summary>
+    /// getter that returns the window object
+    /// </summary>
     public GameObject window
     {
         get
